e3dda1ac8258e68c9c564ab9f89efdbc405f4c05
manav <manavmandli2990@gmail.com>
2024-04-08 18:57:36 +0530

    lates update

--- drive_attachment/drive_attachment/doctype/google_drive_settings/google_drive_settings.js
+++ drive_attachment/drive_attachment/doctype/google_drive_settings/google_drive_settings.js
@@ -1,8 +1,24 @@
 // Copyright (c) 2024, manav and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Google Drive Settings", {
-// 	refresh(frm) {
+frappe.ui.form.on("Google Drive Settings", {
+	authorize(frm) {
+		console.log("call");
+		// frm.call({
+		// 	method: "authoriztion",
+		// 	doc: frm.doc,
+		// 	callback: function (r) {},
+		// });
 
-// 	},
-// });
+		frappe.call({
+			method: "authorize_access",
+			doc: frm.doc,
+			callback: function (r) {
+				if (!r.exc) {
+					frm.save();
+					window.open(r.message.url);
+				}
+			},
+		});
+	},
+});
--- drive_attachment/public/js/drive_file_uploader.js
+++ drive_attachment/public/js/drive_file_uploader.js
@@ -1,24 +1,24 @@
-frappe.provide("Drive Attachment");
+// frappe.provide("Drive Attachment");
 
-drive_attachment.drive_file_uploader = {
-	onload: function (frm) {
-		frappe.ui.form.on("Drive File Uploader", {
-			onload: function (frm) {
-				frm.page.set_primary_action(__("Submit"), function () {
-					frappe.call({
-						method: "drive_attachment.drive_file_uploader.upload_to_drive",
-						args: {},
-						callback: function (response) {
-							var authentication_url = response.message;
-							if (authentication_url) {
-								window.open(authentication_url, "_blank");
-							} else {
-								frappe.msgprint("Error: Authentication URL not found.");
-							}
-						},
-					});
-				});
-			},
-		});
-	},
-};
+// drive_attachment.drive_file_uploader = {
+// 	onload: function (frm) {
+// 		frappe.ui.form.on("Drive File Uploader", {
+// 			onload: function (frm) {
+// 				frm.page.set_primary_action(__("Submit"), function () {
+// 					frappe.call({
+// 						method: "drive_attachment.drive_file_uploader.upload_to_drive",
+// 						args: {},
+// 						callback: function (response) {
+// 							var authentication_url = response.message;
+// 							if (authentication_url) {
+// 								window.open(authentication_url, "_blank");
+// 							} else {
+// 								frappe.msgprint("Error: Authentication URL not found.");
+// 							}
+// 						},
+// 					});
+// 				});
+// 			},
+// 		});
+// 	},
+// };
